88d0cf322a1e7a502bf1bfeedde467d1bf65ba9f
Benjamin Baka <baka@mpedigree.net>
2017-07-07 23:20:17 +0000

    Ben: Referenced the proper classes and used ModelAndView to define the name of the jsp file and the data to go with it

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -5,9 +5,14 @@
  */
 package com.controllers;
 
+import interfaces.ContactDAO;
+import java.util.List;
+import models.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -16,9 +21,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
     
-    @RequestMapping(value="/home", method=RequestMethod.GET)
-    public String viewHome(){
-        return "home";
+    @Autowired
+    private ContactDAO contactDAO;
+    
+    @RequestMapping(value="/homes")
+    public ModelAndView viewHome(ModelAndView model){
+        List<Contact> listContact = contactDAO.listAllContacts();
+        for (Contact listContact1 : listContact) {
+            System.out.println(listContact1.getName());
+            
+        }
+        model.addObject("listContact", listContact);
+        model.setViewName("home");
+        return model;
+
     }
     
 }
